30b682169fefa98ce30eb76f385b980528c59d3b
Griffin <ffirgn@gmail.com>
2022-11-29 17:02:48 -0800

    starting js file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Variables for API
+var apiURL = 'https://api.openweathermap.org';
+var apiKey = '071cd2c4562bcbc21319f59ac6f628f9'
+
+var cityHistory = [];
+
+var searchForm = document.querySelector('#city-search');
